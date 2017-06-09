9ab78506b4f06da1964fc714983ad7d68a56b754
Newt Industries <info@newtindustries.com>
2017-06-09 13:15:14 -0500

    Logging and configuration.
    Adding logging and configuration setup.

--- src/Eft.Core/Configuration/RethinkClientConfig.cs
+++ src/Eft.Core/Configuration/RethinkClientConfig.cs
@@ -1,22 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using RethinkDb.Driver.Net.Clustering;
-
+using Microsoft.Extensions.Configuration;
 namespace Eft.Core.Configuration
 {
-    public class RethinkClientConfig
+    public class RethinkClientConfig 
     {
         private IEnumerable<IPAddress> ipAddresses;
         private IPoolingStrategy poolingStrategy;
         private bool discover;
-
+        private IConfigurationRoot root;
 
         public RethinkClientConfig()
         {
-            
+            var builder =
+                new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("rethinksettings.json");
+            root = builder.Build();
         }
     }
 }
--- src/Eft.Core/Interest/IInterestCriteria.cs
+++ src/Eft.Core/Interest/IInterestCriteria.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Eft.Core.ECS;
 
 namespace Eft.Core.Interest
 {
     public interface IInterestCriteria
     {
-
+        bool IsInterested(Entity source, Entity target);
     }
 }
