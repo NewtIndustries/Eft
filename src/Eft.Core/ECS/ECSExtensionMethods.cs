4608023c98ce19dcd199378bc29bccddfabd1294
Newt Industries <info@newtindustries.com>
2017-06-26 12:57:23 -0500

    Added extension methods for ECS system container.

---
+++ src/Eft.Core/ECS/ECSExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eft.Core.ECS
+{
+    public static class ECSExtensionMethods
+    {
+        #region Entity
+        #endregion
+        #region Component
+
+        public static bool Save(this Component component)
+        {
+            return false;
+        }
+        #endregion
+        #region System
+        #endregion
+    }
+}
